1779156b92de9ee11c3d24041f0021ddbd42606a
Enodi Audu <enodiaudu5@gmail.com>
2020-01-05 23:17:35 +0100

    implement logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,31 @@
 import React from "react";
-import { Typography, Col } from "antd";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Typography, Col, Button } from "antd";
 
+import { startLogout } from "actions/auth";
 import "./header.css";
 
 const { Title } = Typography;
 
-const Header: React.FC = props => (
+export const Header = ({ startLogout }) => (
   <header className="header">
     <Col offset={1}>
-      <Title level={2} type="secondary">
-        Githabb
-      </Title>
+      <Link to="/issues">
+        <Title level={2} type="secondary">
+          Githabb
+        </Title>
+      </Link>
     </Col>
+    <Button onClick={startLogout}>Logout</Button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = dispatch => ({
+  startLogout: () => dispatch(startLogout())
+});
+
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(Header);
