597f4ee842b79f3aef806e4d6d2c9e11e8d1d445
Enodi Audu <enodiaudu5@gmail.com>
2020-01-06 12:28:05 +0100

    remove unused lifecycle method

--- src/components/NewIssue/index.tsx
+++ src/components/NewIssue/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { Input, Button, Row, Form, notification, Icon } from "antd";
 import { connect } from "react-redux";
